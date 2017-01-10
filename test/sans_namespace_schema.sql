269bb1abb49b00f7118285c6f946a7ee221aa935
Mengxuan <xiamx2004@gmail.com>
2017-01-10 15:13:19 -0500

    wip

--- test/sans_namespace_schema.sql
+++ test/sans_namespace_schema.sql
@@ -1,20 +1,8 @@
 DROP SCHEMA IF EXISTS sans_namespace CASCADE;
 CREATE SCHEMA sans_namespace;
 
-DROP TABLE IF EXISTS sans_namespace.users1;
-CREATE TABLE sans_namespace.users1 (
-  email text NOT NULL,
-  id bigint NOT NULL
-);
-
-DROP TABLE IF EXISTS sans_namespace.users2;
-CREATE TABLE sans_namespace.users2 (
-  email text NOT NULL,
-  id bigint NOT NULL
-);
-
-DROP TABLE IF EXISTS sans_namespace.users3;
-CREATE TABLE sans_namespace.users3 (
+DROP TABLE IF EXISTS sans_namespace.users;
+CREATE TABLE sans_namespace.users (
   email text NOT NULL,
   id bigint NOT NULL
 );
